3833c030525a28a54a25b9cda9c3900c5b1f640c
Dokjolly0 <alexviolattolibero.it@gmail.com>
2025-07-15 15:12:32 +0200

    Add stats routes

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -3,11 +3,13 @@ import authRouter from "./auth/auth.router";
 import userRouter from "./user/user.router";
 import requestRouter from "./requests/requests.router";
 import categoryRouter from "./categories/categories.router";
+import * as requestController from "./requests/requests.controller";
 
 const router = express.Router();
 router.use("/users", userRouter);
 router.use("/requests", requestRouter);
 router.use("/categories", categoryRouter);
+router.use("/stats/approved-requests", requestController.getApprovedStats);
 router.use(authRouter);
 
 export default router;
